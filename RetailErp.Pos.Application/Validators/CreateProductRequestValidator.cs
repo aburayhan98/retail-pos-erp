413c3cc772143b9b0db377c6f2478e0c371347b1
Md. Abu Rayhan <aburayhan049596@gmail.com>
2026-04-30 17:53:21 +0600

    added Validators

---
+++ RetailErp.Pos.Application/Validators/CreateProductRequestValidator.cs
@@ -0,0 +1,31 @@
+﻿using FluentValidation;
+using RetailErp.Pos.Application.DTOs.Products;
+
+namespace RetailErp.Pos.Application.Validators;
+
+public sealed class CreateProductRequestValidator
+		: AbstractValidator<CreateProductRequest>
+{
+	public CreateProductRequestValidator()
+	{
+		RuleFor(x => x.Name)
+				.NotEmpty()
+				.WithMessage("Product name is required.")
+				.MaximumLength(100)
+				.WithMessage("Product name must not exceed 100 characters.");
+
+		RuleFor(x => x.Barcode)
+				.NotEmpty()
+				.WithMessage("Barcode is required.")
+				.MaximumLength(50)
+				.WithMessage("Barcode must not exceed 50 characters.");
+
+		RuleFor(x => x.Price)
+				.GreaterThan(0)
+				.WithMessage("Price must be greater than zero.");
+
+		RuleFor(x => x.StockQuantity)
+				.GreaterThanOrEqualTo(0)
+				.WithMessage("Stock quantity cannot be negative.");
+	}
+}
---
+++ RetailErp.Pos.Application/Validators/CreateSalesRequestValidator.cs
@@ -0,0 +1,49 @@
+﻿using FluentValidation;
+using RetailErp.Pos.Application.DTOs.Sales;
+
+namespace RetailErp.Pos.Application.Validators;
+
+public sealed class CreateSalesRequestValidator
+		: AbstractValidator<CreateSaleRequest>
+{
+	public CreateSalesRequestValidator()
+	{
+		RuleFor(x => x.OutletId)
+				.NotEmpty()
+				.WithMessage("OutletId is required.");
+
+		RuleFor(x => x.Items)
+				.NotNull()
+				.WithMessage("Sale items are required.")
+				.Must(items => items.Count > 0)
+				.WithMessage("Sale must contain at least one item.");
+
+		RuleForEach(x => x.Items)
+				.SetValidator(new CreateSaleItemRequestValidator());
+	}
+}
+
+public sealed class CreateSaleItemRequestValidator
+		: AbstractValidator<CreateSaleItemRequest>
+{
+	public CreateSaleItemRequestValidator()
+	{
+		RuleFor(x => x.ProductId)
+				.NotEmpty()
+				.WithMessage("ProductId is required.");
+
+		RuleFor(x => x.Barcode)
+				.NotEmpty()
+				.WithMessage("Barcode is required.")
+				.MaximumLength(100)
+				.WithMessage("Barcode must not exceed 100 characters.");
+
+		RuleFor(x => x.Quantity)
+				.GreaterThan(0)
+				.WithMessage("Quantity must be greater than zero.");
+
+		RuleFor(x => x.UnitPrice)
+				.GreaterThan(0)
+				.WithMessage("Unit price must be greater than zero.");
+	}
+}
