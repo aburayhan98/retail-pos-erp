1e127a5d030acf74ba36385ccaf329a1c6a757d4
Md. Abu Rayhan <aburayhan049596@gmail.com>
2026-04-30 12:17:22 +0600

    setup connectionString

--- RetailErp.Pos.Infrastructure/Data/IDbConnectionFactory.cs
+++ RetailErp.Pos.Infrastructure/Data/IDbConnectionFactory.cs
@@ -1,12 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Data;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
 
-namespace RetailErp.Pos.Infrastructure.Data
+namespace RetailErp.Pos.Infrastructure.Data;
+
+public interface IDbConnectionFactory
 {
-	internal class IDbConnectionFactory
+	Task<IDbConnection> CreateConnectionAsync();
+}
+
+public sealed class DbConnectionFactory : IDbConnectionFactory
+{
+	private readonly string _connectionString;
+
+	public DbConnectionFactory(IConfiguration configuration)
 	{
+		_connectionString = configuration.GetConnectionString("DefaultConnection")
+				?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+	}
+
+	public async Task<IDbConnection> CreateConnectionAsync()
+	{
+		var connection = new SqlConnection(_connectionString);
+		await connection.OpenAsync();
+		return connection;
 	}
 }
