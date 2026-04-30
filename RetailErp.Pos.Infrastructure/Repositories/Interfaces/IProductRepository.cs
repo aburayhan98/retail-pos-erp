a75af3b847bae170e0253bdadce87bbfd27aab8b
Md. Abu Rayhan <aburayhan049596@gmail.com>
2026-04-30 11:57:49 +0600

    added IRepository interfaces

--- RetailErp.Pos.Infrastructure/Repositories/Interfaces/IProductRepository.cs
+++ RetailErp.Pos.Infrastructure/Repositories/Interfaces/IProductRepository.cs
@@ -1,6 +1,12 @@
-﻿namespace RetailErp.Pos.Infrastructure.Repositories.Interfaces
+﻿using RetailErp.Pos.Domain.Entities;
+
+namespace RetailErp.Pos.Infrastructure.Repositories.Interfaces
 {
-	internal class IProductRepository
+	public interface IProductRepository
 	{
+		Task CreateAsync(Product product);
+		Task <IReadOnlyList<Product>> GetAllAsync();
+		Task<Product?> GetByIdAsync(Guid productId);
+		Task ReduceStockAsync(Guid productId, int quantity);
 	}
 }
--- RetailErp.Pos.Infrastructure/Repositories/Interfaces/ISaleRepository.cs
+++ RetailErp.Pos.Infrastructure/Repositories/Interfaces/ISaleRepository.cs
@@ -1,5 +1,14 @@
-﻿namespace RetailErp.Pos.Infrastructure.Repositories.Interfaces;
+﻿using RetailErp.Pos.Domain.Entities;
+
+namespace RetailErp.Pos.Infrastructure.Repositories.Interfaces;
 
 public interface ISaleRepository
 {
+	Task CreateAsync(Sale sale);
+	Task<IReadOnlyList<Sale>> GetAllAsync();
+	Task MarkSyncAsync(Guid saleId, bool isSuccess);
+	Task MarkFailedAsync(Guid saleId);
+	Task IncreaseRetryAsync (Guid saleId);
+	Task<bool> ExistsInCentralAsync(Guid saleId);
+	Task InsertCentralSaleAsync(Sale sale);
 }
